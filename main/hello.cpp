9f22e61118a1b7c5a68afe602dbba2671dcf7289
karankalla <73630525+karankalla@users.noreply.github.com>
2023-02-14 19:27:53 +0530

    Update hello.cpp
    change 1

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, pes2ug20cs156\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
